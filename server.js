89a57b540d901d6602179ba3d0751521f742dbb0
Songkrod Thanavorn <Songkrod Thanavorn>
2016-08-01 11:00:01 +0700

    disconnect database

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
-var db = require('./db');
+// var db = require('./db');
 var express = require('./config/express');
 var passport = require('./config/passport');
 
 var app = express();
 var passport = passport();
 
-db.sequelize.sync().then(function() {
-	console.log('Database connected.');
+//db.sequelize.sync().then(function() {
+//	console.log('Database connected.');
 	app.listen( 4000, function() {
 		console.log('Server running at port 4000 on ' + process.env.NODE_ENV);
 	});
-});
+//});
